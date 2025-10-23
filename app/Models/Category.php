fe02b653682d562e4c358b5d4ebc62673f8df7ce
mddirga <madedirga22@gmail.com>
2025-10-23 22:50:39 +0800

    Fillable & ORM Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
 }
