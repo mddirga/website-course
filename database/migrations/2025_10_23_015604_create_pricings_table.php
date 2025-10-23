9d1a9b3557174f64558ef2558f1d3a4101db7de7
mddirga <madedirga22@gmail.com>
2025-10-23 13:36:36 +0800

    Database Table Schema

--- database/migrations/2025_10_23_015604_create_pricings_table.php
+++ database/migrations/2025_10_23_015604_create_pricings_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('pricings', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('duration');
+            $table->integer('price');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
